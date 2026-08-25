cac56bdf3c5731274f61da51aba2aeb262e679d7
Kartikeya <kartikeyayash4@gmail.com>
2026-08-25 10:56:53 +0530

    Project Setup

---
+++ RAASBackend/src/main/java/com/GP51/RAASBackend/RaasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GP51.RAASBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaasBackendApplication.class, args);
+	}
+
+}
---
+++ RAASBackend/src/test/java/com/GP51/RAASBackend/RaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GP51.RAASBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
